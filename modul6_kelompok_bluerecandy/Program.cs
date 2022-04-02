c53bb77824e8e1580d7f29dc82e050b992b3824a
TiveCS <creativecode.id@gmail.com>
2022-04-02 16:26:40 +0700

    menambahkan class TeamMembers1302204090

--- modul6_kelompok_bluerecandy/Program.cs
+++ modul6_kelompok_bluerecandy/Program.cs
@@ -7,8 +7,11 @@ namespace modul6_kelompok_bluerecandy
 		static void Main(string[] args)
 		{
 			// Demo data mahasiswa 1302204090
-			DataMahasiswa1302204090 d = new DataMahasiswa1302204090();
-			d.ReadJSON();
+			//DataMahasiswa1302204090 d = new DataMahasiswa1302204090();
+			//d.ReadJSON();
+
+			TeamMembers1302204090 t = new TeamMembers1302204090();
+			t.ReadJSON(); 
 		}
 	}
 }
