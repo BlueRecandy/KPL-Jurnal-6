63f4b620002d81a3fac2a69729e6e60ae39feace
luttfimohamad <luttfimohamad@gmail.com>
2022-04-02 17:01:10 +0700

    menambahkan class GlossaryItem_1302204066

--- modul6_kelompok_bluerecandy/Program.cs
+++ modul6_kelompok_bluerecandy/Program.cs
@@ -8,9 +8,10 @@ namespace modul6_kelompok_bluerecandy
 		{
 			DataMahasiswa_1302204066 data = new DataMahasiswa_1302204066();
 			data.ReadJSON();
-
 			TeamMembers_1302204066 data2 = new TeamMembers_1302204066();
 			data2.ReadJSON();
+			GlossaryItem_1302204066 data3 = new GlossaryItem_1302204066();
+			data3.ReadJSON();
 		}
 	}
 }
