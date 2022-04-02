f88bb6268502ef69526e31be0022078e16659d24
iqbalFatur87 <iqbalfatur18@gmail.com>
2022-04-02 16:54:03 +0700

    menambah dan mengedit isi JSON

---
+++ modul6_kelompok_bluerecandy/DataMahasiswa1302200016.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_bluerecandy
+{
+    internal class DataMahasiswa1302200016
+    {
+
+    }
+}
