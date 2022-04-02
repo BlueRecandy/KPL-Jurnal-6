188bf4ff200fa5acf955baba949db1a50ab1b714
JUANDA SINURAT <juandasinurat@365.telkomuniversity.ac.id>
2022-04-02 17:39:13 +0700

    menambahkan class

--- modul6_kelompok_bluerecandy/TeamMembers1302201136.cs
+++ modul6_kelompok_bluerecandy/TeamMembers1302201136.cs
@@ -10,18 +10,23 @@ namespace modul6_kelompok_bluerecandy
 {
     internal class TeamMembers1302201136
     {
-		private string GetJsonPath => Path.GetFullPath(Directory.GetCurrentDirectory() + "/../../../jurnal6_2_1302201136.json");
+        private string GetJsonPath => Path.GetFullPath(Directory.GetCurrentDirectory() + "/../../../jurnal6_2_1302201136.json");
 
-		public void ReadJSON()
-		{
-			var file = File.OpenText(GetJsonPath);
-			JsonElement json = JsonSerializer.Deserialize<JsonElement>(file.ReadToEnd());
+        public void ReadJSON()
+        {
+            var file = File.OpenText(GetJsonPath);
+            JsonElement json = JsonSerializer.Deserialize<JsonElement>(file.ReadToEnd());
 
-			Console.WriteLine("Team member list:");
-			JsonElement members = json.GetProperty("members");
+            Console.WriteLine("Team member list:");
+            JsonElement members = json.GetProperty("members");
 
-			foreach (JsonElement member in members.EnumerateArray())
-			{
-				Console.WriteLine($"{member.GetProperty("nim")} {member.GetProperty("firstName")} {member.GetProperty("lastName")} ({member.GetProperty("age")}, {member.GetProperty("gender")})");
-			}
+            foreach (JsonElement member in members.EnumerateArray())
+            {
+                Console.WriteLine($"{member.GetProperty("nim")} {member.GetProperty("firstName")} {member.GetProperty("lastName")} ({member.GetProperty("age")}, {member.GetProperty("gender")})");
+            }
+        }
+
+    }
 }
+
+
